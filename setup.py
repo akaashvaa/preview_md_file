f02441399261e87b098d6c0089cd2a676126d592
AkashKumar-0 <ahmvaad@gmail.com>
2024-08-31 19:11:54 +0530

    up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='md_preview022',
-    version='1.0.0',
+    version='1.0.1',
     author='Akash Verma',
     author_email='ahmvaad@gmail.com',
     description='A simple Markdown preview tool for the terminal.',
